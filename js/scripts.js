1dc7b111e9fe0feb2755e4011b41bb6ea85cdeab
TakiKauser <takikauserbadhon@gmail.com>
2021-08-17 02:04:49 +0600

    simple dom merged

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// console.log("Connected!");
+document.getElementById("banner-btn").addEventListener("click", function(){
+    const item = document.getElementById("focused-header-text");
+    item.innerText = "I'm the PENGUINE!";
+    item.style.color = "steelblue";
+});
